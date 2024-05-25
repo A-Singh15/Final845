7150cbfc8a0cf8611b669a255a42779ce53de939
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 17:55:17 -0700

    Update top_tb.sv

--- top_tb.sv
+++ top_tb.sv
@@ -2,39 +2,45 @@
 
 `include "defines.sv"
 `include "interface.sv"
-//`include "generator.sv"
+`include "generator.sv"
 `include "test.sv"
 //`include "assertion.sv"
 
 module top_tb();
   // Clock Generation
-  bit               clk;  
+  bit clk;  
   always #10 clk = ~clk;   
-    initial begin 
+  
+  initial begin 
+    clk = 0;
     $display(" ================================================= TB Start = 0 =================================================\n");
     topif.start = 1'b0;
-    repeat(2)
-    @(posedge clk);   
+    repeat(2) @(posedge clk);   
     topif.start = 1'b1;
   end              
   
   // Interface Instantiation
   top_if topif(clk); 
 
-  //Memory instantiation          
+  // Memory instantiation          
   ROM_R memR_u(.clock(clk), .AddressR(topif.AddressR), .R(topif.R));
   ROM_S memS_u(.clock(clk), .AddressS1(topif.AddressS1), .AddressS2(topif.AddressS2), .S1(topif.S1), .S2(topif.S2));
   
-  assign memR_u.Rmem = topif.R_mem;
-  assign memS_u.Smem = topif.S_mem;
+  initial begin
+    // Initialize memory
+    for (int i = 0; i < `RMEM_MAX; i++) begin
+      memR_u.Rmem[i] = 0;
+    end
+    for (int i = 0; i < `SMEM_MAX; i++) begin
+      memS_u.Smem[i] = 0;
+    end
+  end
 
-  // Test
+  // Test Instantiation
+  test test_u(.topif(topif));       
 
-  test test(topif);       
-
-   //DUT Instantiation
-
-  top dut(                             
+  // DUT Instantiation
+  top dut(
     .clock(topif.clk), 
     .start(topif.start), 
     .BestDist(topif.BestDist), 
@@ -46,6 +52,7 @@ module top_tb();
     .R(topif.R), 
     .S1(topif.S1), 
     .S2(topif.S2), 
-    .completed(topif.completed));
+    .completed(topif.completed)
+  );
 
 endmodule
