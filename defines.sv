fb0a51fd806665124ba57e47ee0247f86aeb569c
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 17:50:36 -0700

    Update defines.sv

--- defines.sv
+++ defines.sv
@@ -3,6 +3,6 @@
 
 `define SMEM_MAX 1024
 `define RMEM_MAX 256
-`define TRANSACTION_COUNT 1
+`define TRANSACTION_COUNT 100
 `define DRIV_IF topif.ME_DRIVER.ME_driver_cb
 `define MON_IF topif.ME_MONITOR.ME_monitor_cb
